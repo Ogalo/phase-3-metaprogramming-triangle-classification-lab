8c0c6aa243afb637487568bbd38aec5b2775251d
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-25 20:00:01 +0300

    commit tree ec07326cc6ee67ca84659e88c9c7738c3980f3c6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1=side1
+    @side2=side2
+    @side3=side3
+  end
+
+  def kind
+    if self.side2 <= 0 || self.side1 <= 0 || self.side3 <= 0
+      raise TriangleError
+
+    elsif self.side2 == self.side3 && self.side3 == self.side1
+      :equilateral
+    elsif self.side2 == self.side3 || self.side3 == self.side1 || self.side1 == self.side2
+      :isosceles
+    elsif self.side1 != self.side3 && self.side3 != self.side1 && self.side1 != self.side2
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a valid triangle"
+    end
+
+  end
+
 end
+# test = Triangle.new(1,1,3)
+# puts sample.kind
